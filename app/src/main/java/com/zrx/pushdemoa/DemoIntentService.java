88e6b2c87fe17f54df4a1b755b16cde958675546
ZRXiang <1441721645@qq.com>
2017-03-23 16:29:51 +0800

    Initial commit

---
+++ app/src/main/java/com/zrx/pushdemoa/DemoIntentService.java
@@ -0,0 +1,40 @@
+package com.zrx.pushdemoa;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.igexin.sdk.GTIntentService;
+import com.igexin.sdk.message.GTCmdMessage;
+import com.igexin.sdk.message.GTTransmitMessage;
+
+/**
+ * Created by 张茹祥 on 2017/3/22.
+ */
+
+public class DemoIntentService extends GTIntentService {
+
+    public DemoIntentService() {
+
+    }
+
+    @Override
+    public void onReceiveServicePid(Context context, int pid) {
+    }
+
+    @Override
+    public void onReceiveMessageData(Context context, GTTransmitMessage msg) {
+    }
+
+    @Override
+    public void onReceiveClientId(Context context, String clientid) {
+        Log.e(TAG, "onReceiveClientId -> " + "clientid = " + clientid);
+    }
+
+    @Override
+    public void onReceiveOnlineState(Context context, boolean online) {
+    }
+
+    @Override
+    public void onReceiveCommandResult(Context context, GTCmdMessage cmdMessage) {
+    }
+}
